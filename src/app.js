f1d47d222d9906ed1d46c23efa52ad3935261bae
Andrew Matheus <andrewmatheus@gmail.com>
2019-10-27 09:25:56 -0300

    ID3: Adicionando controllers Session e User;

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import routes from './routes';
 
+import './config/database';
 import './database';
 
 class App {
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,17 @@
 import { Router } from 'express';
 
-// import UserController from './app/controllers/UserController';
-// import SessionController from './app/controllers/SessionController';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
-// import authMiddleware from './app/middleware/auth';
+import authMiddleware from './app/middleware/auth';
 
 const routes = new Router();
 
-// routes.post('/users', UserController.store);
-// routes.post('/sessions', SessionController.store);
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
-// routes.use(authMiddleware);
+routes.use(authMiddleware);
 
-// routes.put('/users', UserController.update);
+routes.put('/users', UserController.update);
 
 export default routes;
